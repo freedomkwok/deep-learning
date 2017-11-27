cdd6d04254b05be12e210164b52dd2ad4bab2eca
RG <freedomkwok@hotmail.com>
2017-11-27 02:11:46 -0800

    RG master

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-
-def error_formula(y, output):
-    return - y * np.log(output) - (1 - y) * np.log(1 - output)
-
 class NeuralNetwork(object):
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
         # Set number of nodes in input, hidden and output layers.
@@ -22,10 +18,10 @@ class NeuralNetwork(object):
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x: 1.0 / (1 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
+        self.activation_function = lambda x: 1 / (1 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
 
         ### If the lambda code above is not something you're familiar with,
-        # You can uncomment out the following three lines and put your 
+        # You can uncomment out the following three lines and put your
         # implementation there instead.
         #
         #def sigmoid(x):
@@ -33,11 +29,11 @@ class NeuralNetwork(object):
         #self.activation_function = sigmoid
 
     def train(self, features, targets):
-        ''' Train the network on batch of features and targets. 
-        
+        ''' Train the network on batch of features and targets.
+
             Arguments
             ---------
-            
+
             features: 2D array, each row is one data record, each column is a feature
             targets: 1D array of target values
         
@@ -64,19 +60,20 @@ class NeuralNetwork(object):
         ### Forward pass ###
         print(np.array([X]).shape, self.weights_input_to_hidden.shape)
         # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = np.dot(np.array([X]), self.weights_input_to_hidden)
+        hidden_inputs = np.array([X])
 
         print(hidden_inputs.shape)
         # signals into hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs)
+        hidden_outputs = np.dot(hidden_inputs, self.weights_input_to_hidden)
         # signals from hidden layer
+        print('hidden_outputs', hidden_outputs.shape, 'weights_hidden_to_output',self.weights_hidden_to_output.shape)
 
-        print(hidden_outputs.shape, self.weights_hidden_to_output.shape)
         # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
+        final_inputs = self.activation_function(np.dot(hidden_outputs, self.weights_hidden_to_output)) # signals into final output layer
         final_outputs = final_inputs # signals from final output layer
-        
-        return final_outputs, final_inputs
+
+        print('final_outputs', final_outputs, 'hidden_outputs', hidden_outputs)
+        return final_outputs, hidden_outputs
 
     def backpropagation(self, final_outputs, hidden_outputs, X, y, delta_weights_i_h, delta_weights_h_o):
         ''' Implement backpropagation
@@ -94,29 +91,31 @@ class NeuralNetwork(object):
 
         print("final_outputs", final_outputs.shape)
         # TODO: Output error - Replace this value with your calculations.
-        error = y - final_outputs # Output layer error is the difference between desired target and actual output.
+        error = final_outputs - y # Output layer error is the difference between desired target and actual output.
 
         # TODO: Calculate the hidden layer's contribution to the error
 
         # TODO: Backpropagated error terms - Replace these values with your calculations.
         # (y - final_outputs) * * final_outputs * (1 - final_outputs)
-        output_error_term = error * (final_outputs * (1 - final_outputs)) #1x1
+        output_error_term = error * (final_outputs * (1 - final_outputs)) #1x1 //layer 2 delta
 
         print("output_error_term", output_error_term.shape)
 
-        print("weights_hidden_to_output", self.weights_hidden_to_output.shape, output_error_term.shape)
-        hidden_error = np.dot(self.weights_hidden_to_output, output_error_term) #2x1
+        print("weights_hidden_to_output", output_error_term.shape,
+              self.weights_hidden_to_output.T.shape)
+        hidden_error = np.dot(output_error_term, self.weights_hidden_to_output.T) #2x1
 
         print("hidden_error",hidden_error.shape, "hidden_outputs", hidden_outputs.shape)
         #hidden_error_term= np.dot(self.weights_hidden_to_output, output_error_term)* hidden_outputs * (1 - hidden_outputs)
-        hidden_error_term = hidden_error * (hidden_outputs * (1 - hidden_outputs)) #2x1
+        hidden_error_term = hidden_error #2x1
 
-        print("np.array([X])", np.array([X]).shape, "hidden_error_term", hidden_error_term.shape)
-        # Weight step (input to hidden)
-        delta_weights_i_h += np.dot(hidden_error_term, np.array([X])).T #3x2
-
+        print("final_outputs", final_outputs.shape, "output_error_term", output_error_term.shape)
         # Weight step (hidden to output)
-        delta_weights_h_o += output_error_term * hidden_outputs #2x1
+        delta_weights_h_o += np.dot(hidden_outputs.T, output_error_term)#2x1
+
+        print("np.array([X])", np.array([X]).T.shape, "hidden_error_term", hidden_error_term.shape)
+        # Weight step (input to hidden)
+        delta_weights_i_h += np.dot(np.array([X]).T, hidden_error_term) #3x2
 
         return delta_weights_i_h, delta_weights_h_o
 
