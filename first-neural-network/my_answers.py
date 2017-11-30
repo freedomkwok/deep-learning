5947290b6a21d8299208bf203d2448d498f86bd0
RG <freedomkwok@hotmail.com>
2017-11-29 21:08:21 -0800

    RG master

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -153,7 +153,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 600
-learning_rate = 0.035
-hidden_nodes = 2
+iterations = 6000
+learning_rate = 0.5
+hidden_nodes = 12
 output_nodes = 1
